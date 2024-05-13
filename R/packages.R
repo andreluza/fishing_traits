d9e27b10acb2ebf1577ea27e6ce4202aaec9e1ed
andreluza <luza.andre@gmail.com>
2024-05-13 22:01:51 +0200

    packages and functions

---
+++ R/packages.R
@@ -0,0 +1,4 @@
+# load
+require(ks);require(hypervolume);require(vegan);require(reshape);require(gawdis);require(ape);require(reshape);require(reshape2)
+require(gridExtra);require(grid); require(ggplot2); require(lattice); require(dplyr);
+require(here)
